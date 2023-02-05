2d7ddbea449403d1c0bc004904b421fd89baed90
kaimikan <oihic@protonmail.com>
2023-02-05 14:03:37 +0200

    Day 26 - NATO Alphabet Converter

---
+++ 26-list-comprehension-and-nato-alphabet/nato-alphabet/main.py
@@ -0,0 +1,23 @@
+import pandas
+
+# Step 1: Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(nato_alphabet)
+nato_alphabet_dict = {}
+# Loop through rows of a data frame
+for (index, row) in nato_alphabet.iterrows():
+    # Access index and row
+    # print(row.letter, row.code)
+    nato_alphabet_dict[row.letter] = row.code
+    # Access row.student or row.score
+    pass
+
+print(nato_alphabet_dict)
+
+# Step 2: Create a list of the phonetic code words from a word that the user inputs.
+user_word = input("Enter a word to get it transcribed to phonetic code: ")
+word_transcription = []
+for letter in user_word:
+    word_transcription.append(nato_alphabet_dict[letter.upper()])
+
+print(f"The transcription for {user_word} is: {word_transcription}")
